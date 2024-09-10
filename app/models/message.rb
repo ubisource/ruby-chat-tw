1fc9571a436e557261b7b06aee57c8328d91e13f
Rubyalam MS <ubisource@gmail.com>
2024-09-10 11:25:51 +0700

    update pages and messages controller

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,22 @@
 class Message < ApplicationRecord
   belongs_to :user
+  validates :body, presence: true, length: { maximum: 500 }
+
+  after_create_commit :broadcast_create
+  after_destroy_commit :broadcast_destroy
+
+  def broadcast_create
+    broadcast_append_to "messages",
+                        partial: "messages/message",
+                        locals: { message: self, display_controls: false }
+
+    broadcast_replace_to "user_#{self.user.id}",
+                         target: "message_#{self.id}_controls",
+                         partial: "messages/controls",
+                         locals: { message: self }
+  end
+
+  def broadcast_destroy
+    broadcast_remove_to "messages"
+  end
 end

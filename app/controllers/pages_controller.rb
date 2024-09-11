0cfc6227aad71a43930f980abc21c5d1a6c968e3
Rubyalam MS <ubisource@gmail.com>
2024-09-11 15:19:09 +0700

    change template room and messages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def home
-    @messages = Message.last(5)
-    @message = current_user.messages.build
+    @rooms = Room.all
+    @messages = Message.all
+    # @message = current_user.messages.build
   end
 end
--- db/migrate/20240910031240_create_messages.rb
+++ db/migrate/20240910031240_create_messages.rb
@@ -3,6 +3,7 @@ class CreateMessages < ActiveRecord::Migration[7.2]
     create_table :messages do |t|
       t.text :body
       t.references :user, null: false, foreign_key: true
+      t.references :room, null: true, foreign_key: true
 
       t.timestamps
     end
